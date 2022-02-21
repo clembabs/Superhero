da33a6c302362e7d2e8c4aea46cbe338fa5587a6
Clement Babaniyi <clembabs10@gmail.com>
2022-02-21 21:34:26 +0100

    Add routes

--- lib/app/routes.dart
+++ lib/app/routes.dart
@@ -1,4 +1,6 @@
 class Routes {
   // General
   static const String base = '/';
+  static const String splashScreen = base + 'splashScreen/';
+  static const String checkInternet = base + 'checkInternet/';
 }
